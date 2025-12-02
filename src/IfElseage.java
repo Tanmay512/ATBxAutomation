fb47ed2bb3950c86a53894c2a7797cc72c181c3c
Tanmay <dastanmay260@gmail.com>
2025-12-02 13:07:10 +0530

    Test

---
+++ src/IfElseage.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class IfElseage {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Age");
+        int age = sc.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Eligible for Vote");
+        }
+
+            else{
+            System.out.println(" Not Eligible for Vote");
+        }
+
+
+    }
+
+
+
+    }
+
+
