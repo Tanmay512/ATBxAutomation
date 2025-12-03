8a3fdaf752c055faf2f251dffadd0e24bba77893
Tanmay <dastanmay260@gmail.com>
2025-12-03 15:09:43 +0530

    Test

--- src/Ex_Switch/Ex1_Switch.java
+++ src/Ex_Switch/Ex1_Switch.java
@@ -11,16 +11,22 @@ public class Ex1_Switch {
         switch (day){
             case 1:
                 System.out.println("Mon");
+                break;
             case 2:
                 System.out.println("Tue");
+                break;
             case 3:
                 System.out.println("Wed");
+                break;
             case 4:
                 System.out.println("Thu");
+                break;
             case 5:
                 System.out.println("Fri");
+                break;
             case 6:
                 System.out.println("Sat");
+                break;
             case 7:
                 System.out.println("Sun");
                 break;
--- src/Ex_Switch/Interview03Switch.java
+++ src/Ex_Switch/Interview03Switch.java
@@ -1,5 +1,13 @@
 package Ex_Switch;
 
 public class Interview03Switch {
+    public static void main(String[] args) {
+        long l2 = 8112L;
+        switch ((int)l2){
+
+
+        }
+    }
 
 }
+
