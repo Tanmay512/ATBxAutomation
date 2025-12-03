80fcf9e4956c967f4ff4c834d8823743e36d3c3a
Tanmay <dastanmay260@gmail.com>
2025-12-03 15:09:34 +0530

    Test

---
+++ src/Ex_Switch/InterviewJDK13.java
@@ -0,0 +1,21 @@
+package Ex_Switch;
+
+public class InterviewJDK13 {
+    static void main(String[] args) {
+        int T1 = 005;
+        switch (T1){
+            case 001:
+            System.out.println("001");
+            break;
+            case 002:
+                System.out.println("002");
+                break;
+            case 003:
+                System.out.println("003");
+                break;
+            default:
+                System.out.println("Not Mention");
+
+        }
+    }
+}
