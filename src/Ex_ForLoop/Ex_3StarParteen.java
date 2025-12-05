150bd85cd6aaf52a6d17a0fffbc13bda725c19dd
Tanmay <dastanmay260@gmail.com>
2025-12-05 11:44:32 +0530

    Test....

--- src/Ex_ForLoop/Ex_3StarParteen.java
+++ src/Ex_ForLoop/Ex_3StarParteen.java
@@ -2,53 +2,11 @@ package Ex_ForLoop;
 
 public class Ex_3StarParteen {
     static void main(String[] args) {
-        //for (int i=1;i<=5;i++){
-       // for (int j=i)
+        for (int i=1;i<=5;i++) {
+            System.out.println(i);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
 
 
 
