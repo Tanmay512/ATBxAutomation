909b8e29b01cbff2f7be4b6389e6efa25cc39d4d
Tanmay <dastanmay260@gmail.com>
2025-12-05 12:21:07 +0530

    Test....

---
+++ src/Ex_ForLoop/Ex_PrintJanToDec.java
@@ -0,0 +1,11 @@
+package Ex_ForLoop;
+
+public class Ex_PrintJanToDec {
+    static void main(String[] args) {
+        String[] months ={"Jan","Feb", "Mar","Apr","May","Jun", "Jul","Aug","Sept","Oct","Nov","Dec"};
+
+        for (int i=0;i< months.length;i++){
+            System.out.println(months[i]);
+        }
+    }
+}
